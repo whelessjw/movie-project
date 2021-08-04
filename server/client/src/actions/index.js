812864b753a953a58456593a88b583a80e6560eb
Jeremy Wheless <whelessjw@gmail.com>
2021-08-04 11:15:49 -0400

    added heroku-postbuild script

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,116 +1,125 @@
 import axios from "axios";
-import { FETCH_MOVIES, FETCH_MOVIE, AUTH_USER, AUTH_ERROR, ADD_MOVIE, FETCH_WATCHLIST_MOVIES } from './types';
+import {
+  FETCH_MOVIES,
+  FETCH_MOVIE,
+  AUTH_USER,
+  AUTH_ERROR,
+  ADD_MOVIE,
+  FETCH_WATCHLIST_MOVIES,
+} from "./types";
 
-export const fetchMovies = (page = 1) => dispatch => {
-  axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=a50dd974dc6bceb5358b37229983facc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
-  ).then(function (response) {
-    dispatch({ type: FETCH_MOVIES, payload: response.data });
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-};
-
-export const fetchMovie = (id) => dispatch => {
-  axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=a50dd974dc6bceb5358b37229983facc&include_adult=false&include_video=false`
-  ).then(function (response) {
-    dispatch({ type: FETCH_MOVIE, payload: response.data });
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-};
-
-export const signup = (formProps, callback) => dispatch => {
-  axios.post(
-    'http://localhost:5000/auth/signup',
-    formProps
-  ).then(function (response) {
-    dispatch({ type: AUTH_USER, payload: response.data });
-    localStorage.setItem('token', response.data.token);
-    callback();
-  })
-  .catch(function (error) {
-    dispatch({ type: AUTH_ERROR, payload: error });
-  });
-};
-
-export const signin = (formProps, callback) => dispatch => {
-  axios.post(
-    'http://localhost:5000/auth/signin',
-    formProps
-  ).then(function (response) {
-    debugger;
-    dispatch({ type: AUTH_USER, payload: response.data });
-    localStorage.setItem('token', response.data.token);
-    callback();
-  })
-  .catch(function (error) {
-    dispatch({ type: AUTH_ERROR, payload: error });
-  });
-};
-
-export const fetchUser = () => dispatch => {
-  const config = {
-    headers: {
-      Authorization: 'Bearer ' + localStorage.getItem('token'),
-    }
+export const fetchMovies =
+  (page = 1) =>
+  (dispatch) => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIE_DB_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+      )
+      .then(function (response) {
+        dispatch({ type: FETCH_MOVIES, payload: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
-  axios.get(
-    'http://localhost:5000/auth/current_user',
-    config
-  ).then(function (response) {
-    dispatch({ type: AUTH_USER, payload: response.data });
-    localStorage.setItem('token', response.data.token);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+export const fetchMovie = (id) => (dispatch) => {
+  axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_MOVIE_DB_KEY}&include_adult=false&include_video=false`
+    )
+    .then(function (response) {
+      dispatch({ type: FETCH_MOVIE, payload: response.data });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
 
-export const signout = (callback) => dispatch => {
-  localStorage.removeItem('token');
-
-  dispatch({ type: AUTH_USER, payload: '' });
-  callback()
+export const signup = (formProps, callback) => (dispatch) => {
+  axios
+    .post("/auth/signup", formProps)
+    .then(function (response) {
+      dispatch({ type: AUTH_USER, payload: response.data });
+      localStorage.setItem("token", response.data.token);
+      callback();
+    })
+    .catch(function (error) {
+      dispatch({ type: AUTH_ERROR, payload: error });
+    });
 };
 
-export const addMovieToWatchList = (movie) => dispatch => {
+export const signin = (formProps, callback) => (dispatch) => {
+  axios
+    .post("/auth/signin", formProps)
+    .then(function (response) {
+      debugger;
+      dispatch({ type: AUTH_USER, payload: response.data });
+      localStorage.setItem("token", response.data.token);
+      callback();
+    })
+    .catch(function (error) {
+      dispatch({ type: AUTH_ERROR, payload: error });
+    });
+};
+
+export const fetchUser = () => (dispatch) => {
+  const config = {
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+  };
+
+  axios
+    .get("/auth/current_user", config)
+    .then(function (response) {
+      dispatch({ type: AUTH_USER, payload: response.data });
+      localStorage.setItem("token", response.data.token);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+export const signout = (callback) => (dispatch) => {
+  localStorage.removeItem("token");
+
+  dispatch({ type: AUTH_USER, payload: "" });
+  callback();
+};
+
+export const addMovieToWatchList = (movie) => (dispatch) => {
   console.log(movie);
-  
+
   const config = {
     headers: {
-      Authorization: 'Bearer ' + localStorage.getItem('token'),
-    }
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
   };
 
-  axios.post(
-    'http://localhost:5000/api/watchlist',
-    { movie },
-    config
-  ).then(function (response) {
-    dispatch({ type: ADD_MOVIE, payload: response.data });
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  axios
+    .post("/api/watchlist", { movie }, config)
+    .then(function (response) {
+      dispatch({ type: ADD_MOVIE, payload: response.data });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
 
-export const fetchWatchListMovies = () => dispatch => {
+export const fetchWatchListMovies = () => (dispatch) => {
   const config = {
     headers: {
-      Authorization: 'Bearer ' + localStorage.getItem('token'),
-    }
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
   };
 
-  axios.get(
-    'http://localhost:5000/api/watchlist',
-    config
-  ).then(function (response) {
-    dispatch({ type: FETCH_WATCHLIST_MOVIES, payload: response.data });
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  axios
+    .get("/api/watchlist", config)
+    .then(function (response) {
+      dispatch({ type: FETCH_WATCHLIST_MOVIES, payload: response.data });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,20 +1,27 @@
 import "./App.css";
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { render } from "react-dom";
 import Nav from "./components/Nav";
-import App from './components/App';
+import App from "./components/App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/index";
-import MovieList from './components/MovieList';
-import MovieDetail from './components/MovieDetail';
-import Signup from './components/auth/Signup';
-import Signin from './components/auth/Signin';
-import WatchListButton from './components/WatchListButton';
+import MovieList from "./components/MovieList";
+import MovieDetail from "./components/MovieDetail";
+import Signup from "./components/auth/Signup";
+import Signin from "./components/auth/Signin";
+import WatchListButton from "./components/WatchListButton";
 
-const store = createStore(rootReducer, {}, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  {},
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 render(
   <Provider store={store}>
@@ -24,14 +31,14 @@ render(
         <App>
           <Switch>
             <Route exact path="/">
-              <MovieList type='discover' />
+              <MovieList type="discover" />
             </Route>
 
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/signin" component={Signin} />
 
             <Route exact path="/watch-list">
-              <MovieList type='watch-list'/>
+              <MovieList type="watch-list" />
             </Route>
 
             <Route exact path="/watch-list/:id" component={MovieDetail} />
